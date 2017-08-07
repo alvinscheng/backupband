0346b3ec3b0a34f778cadd5d46df31fed4cc0148
John Haupenthal <johnhaup@gmail.com>
2017-08-07 14:48:03 -0700

    Add db of drum sounds.  Add Howler dependency. Make kick drum play on page load. #1

--- client/index.js
+++ client/index.js
@@ -1,17 +1,5 @@
-const context = new AudioContext
+const { Howl } = require('howler')
 
-const oscillator = context.createOscillator()
-oscillator.frequency.value = 150
-
-const gain = context.createGain()
-
-oscillator.connect(gain)
-gain.connect(context.destination)
-
-const now = context.currentTime
-
-gain.gain.setValueAtTime(1,now)
-gain.gain.exponentialRampToValueAtTime(0.001, now + 2)
-
-oscillator.start(now)
-oscillator.stop(now + 2)
+var sound = new Howl({
+  src: ['../db/drums/kick.wav']
+}).play();
