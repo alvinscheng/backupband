68c1d59d7f3c177781282262376c97907d72fa6a
John Haupenthal <johnhaup@gmail.com>
2017-08-07 15:29:08 -0700

    Add switch statement to play drum sounds on keydown. #1

--- components/app.js
+++ components/app.js
@@ -11,7 +11,12 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>You rendered something you crazy fool!</h1>
+        <h3>F = Kick Drum</h3>
+        <h3>J = Snare Drum</h3>
+        <h3>E = Hi Hat (closed)</h3>
+        <h3>W = Cymbal</h3>
+        <h3>I = Hi Tom</h3>
+        <h3>O = Kick Drum</h3>
       </div>
     )
   }
