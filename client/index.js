6290e79c02499efd9720685541f13c3838a63fc9
John Haupenthal <johnhaup@gmail.com>
2017-08-07 14:02:53 -0700

    Note fades out in two seconds. #1

--- client/index.js
+++ client/index.js
@@ -1,9 +1,17 @@
 const context = new AudioContext
 
 const oscillator = context.createOscillator()
-oscillator.frequency.value = 261.6
+oscillator.frequency.value = 150
 
-oscillator.connect(context.destination)
+const gain = context.createGain()
 
-oscillator.start(0)
-oscillator.stop(5)
+oscillator.connect(gain)
+gain.connect(context.destination)
+
+const now = context.currentTime
+
+gain.gain.setValueAtTime(1,now)
+gain.gain.exponentialRampToValueAtTime(0.001, now + 2)
+
+oscillator.start(now)
+oscillator.stop(now + 2)
