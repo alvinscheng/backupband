cfbe14496209466788f7cb9edeb57c5875d5616c
John Haupenthal <johnhaup@gmail.com>
2017-08-08 13:20:30 -0700

    Add web-audio-scheduler to dependencies. Add metronome.js in client folder. #2

---
+++ client/components/metronome.js
@@ -0,0 +1,65 @@
+import WebAudioScheduler from 'web-audio-scheduler'
+
+const audioContext = new AudioContext();
+const sched = new WebAudioScheduler({ context: audioContext });
+let masterGain = null;
+
+function metronome(e) {
+  const t0 = e.playbackTime;
+
+  sched.insert(t0 + 0.000, ticktack, { frequency: 880, duration: 1.0 });
+  sched.insert(t0 + 0.500, ticktack, { frequency: 440, duration: 0.1 });
+  sched.insert(t0 + 1.000, ticktack, { frequency: 440, duration: 0.1 });
+  sched.insert(t0 + 1.500, ticktack, { frequency: 440, duration: 0.1 });
+  sched.insert(t0 + 2.000, metronome);
+}
+
+function ticktack(e) {
+  const t0 = e.playbackTime;
+  const t1 = t0 + e.args.duration;
+  const osc = audioContext.createOscillator();
+  const amp = audioContext.createGain();
+
+  osc.frequency.value = e.args.frequency;
+  osc.start(t0);
+  osc.stop(t1);
+  osc.connect(amp);
+
+  amp.gain.setValueAtTime(0.5, t0);
+  amp.gain.exponentialRampToValueAtTime(1e-6, t1);
+  amp.connect(masterGain);
+
+  sched.nextTick(t1, () => {
+    osc.disconnect();
+    amp.disconnect();
+  });
+}
+
+sched.on("start", () => {
+  masterGain = audioContext.createGain();
+  masterGain.connect(audioContext.destination);
+});
+
+sched.on("stop", () => {
+  masterGain.disconnect();
+  masterGain = null;
+});
+
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState === "visible") {
+    sched.aheadTime = 0.1;
+  } else {
+    sched.aheadTime = 1.0;
+    sched.process();
+  }
+});
+
+document.getElementById("start-button").addEventListener("click", () => {
+  sched.start(metronome);
+});
+
+document.getElementById("stop-button").addEventListener("click", () => {
+  sched.stop(true);
+});
+
+module.exports = { metronome, sched, ticktack }
