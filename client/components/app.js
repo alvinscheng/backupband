96eee0aea061178179f4a2b855da7d51d8180526
Nic Bonetto <nick.bonetto@gmail.com>
2017-08-15 15:31:03 -0700

    Created coordinates reducer. #16

--- client/components/app.js
+++ client/components/app.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import keyboard from '../keyboard.js'
 import RenderBand from './band.js'
 import Scroll from 'react-scroll'
+import Keys from './keys'
 
 class App extends React.Component {
   constructor(props) {
@@ -36,6 +37,7 @@ class App extends React.Component {
         />
         <div id="keyboard">
           <img src="./pics/BuBkeyboard.png" />
+          <Keys/>
         </div>
         <div id="text-instructions">
           <p><u>Free Play</u></p>
@@ -49,9 +51,9 @@ class App extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    beat: state.beat,
-    intensity: state.intensity,
-    nextIntensity: state.nextIntensity
+    beat: state.tempo.beat,
+    intensity: state.tempo.intensity,
+    nextIntensity: state.tempo.nextIntensity
   }
 }
 
