b46afed4d2af2d13ebba7664203f515180bf9359
John Haupenthal <johnhaup@gmail.com>
2017-08-16 11:23:09 -0700

    Lightened key highlights.  Show/hide keys added. #16

--- store.js
+++ store.js
@@ -27,39 +27,39 @@ function tempo(state = {beat: 1, intensity: 0, nextIntensity: 0}, action) {
 function coordinates(state = {}, action) {
   switch (action.type) {
     case 'CRASH':
-      return Object.assign({}, {x: 23.3, y: 40.7, height: 11.5, width: 4})
+      return Object.assign({}, {x: 23.3, y: 39.4, height: 11.5, width: 4})
     case 'HIGH_HAT_OPEN':
-      return Object.assign({}, {x: 28, y: 40.8, height: 11.4, width: 4})
+      return Object.assign({}, {x: 28, y: 39.5, height: 11.4, width: 4})
     case 'HIGH_HAT_PEDAL':
-      return Object.assign({}, {x: 32.7, y: 40.9, height: 11.4, width: 4})
+      return Object.assign({}, {x: 32.7, y: 39.6, height: 11.4, width: 4})
     case 'CROSS_STICK':
-      return Object.assign({}, {x: 37.4, y: 40.8, height: 11.4, width: 4})
+      return Object.assign({}, {x: 37.4, y: 39.5, height: 11.4, width: 4})
     case 'CLAP':
-      return Object.assign({}, {x: 51.6, y: 40.8, height: 11.4, width: 3.9})
+      return Object.assign({}, {x: 51.6, y: 39.5, height: 11.4, width: 3.9})
     case 'HIGH_TOM':
-      return Object.assign({}, {x: 56.24, y: 40.8, height: 11.4, width: 4})
+      return Object.assign({}, {x: 56.24, y: 39.5, height: 11.4, width: 4})
     case 'MED_TOM':
-      return Object.assign({}, {x: 60.9, y: 40.8, height: 11.4, width: 4})
+      return Object.assign({}, {x: 60.9, y: 39.5, height: 11.4, width: 4})
     case 'LOW_TOM':
-      return Object.assign({}, {x: 65.6, y: 40.8, height: 11.4, width: 4})
+      return Object.assign({}, {x: 65.6, y: 39.5, height: 11.4, width: 4})
     case 'KICK':
-      return Object.assign({}, {x: 38.6, y: 54.6, height: 11.4, width: 4})
+      return Object.assign({}, {x: 38.6, y: 52.9, height: 11.4, width: 4})
     case 'SNARE':
-      return Object.assign({}, {x: 52.7, y: 54.6, height: 11.4, width: 4})
+      return Object.assign({}, {x: 52.7, y: 52.9, height: 11.4, width: 4})
     case 'INTENSITY_DOWN':
-      return Object.assign({}, {x: 40.9, y: 68.5, height: 11.4, width: 4})
+      return Object.assign({}, {x: 40.9, y: 66.3, height: 11.4, width: 4})
     case 'INTENSITY_UP':
-      return Object.assign({}, {x: 50.4, y: 68.5, height: 11.4, width: 4})
+      return Object.assign({}, {x: 50.4, y: 66.3, height: 11.4, width: 4})
     case 'METRONOME':
-      return Object.assign({}, {x: 36.3, y: 82.3, height: 13.4, width: 22.72})
+      return Object.assign({}, {x: 36.3, y: 79.7, height: 13.4, width: 22.72})
     case 'INTENSITY_DOWN_TWO':
-      return Object.assign({}, {x: 30, y: 20, height: 30, width: 30})
+      return Object.assign({}, {x: 30, y: 18.7, height: 30, width: 30})
     case 'TEMPO_UP':
-      return Object.assign({}, {x: 75, y: 82.7, height: 6.4, width: 4})
+      return Object.assign({}, {x: 75, y: 79.8, height: 6.4, width: 4})
     case 'TEMPO_DOWN':
-      return Object.assign({}, {x: 75, y: 89.2, height: 6.4, width: 4})
+      return Object.assign({}, {x: 75, y: 86.4, height: 6.4, width: 4})
     case 'INTENSITY_UP_TWO':
-      return Object.assign({}, {x: 40, y: 50, height: 30, width: 30})
+      return Object.assign({}, {x: 40, y: 48.7, height: 30, width: 30})
     default:
       return state
   }
@@ -67,7 +67,8 @@ function coordinates(state = {}, action) {
 
 const store = createStore(combineReducers({
   tempo: tempo,
-  coordinates: coordinates
+  coordinates: coordinates,
+  keys: false
 }))
 
 export default store
